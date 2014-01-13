bcefce2eee816813ee474a59ff56d4269430cdf7
bmperez <bmperez@seventy7.(none)>
2014-01-13 18:32:40 -0500

    Updated the script to working condition. Sends 1 and then receives a number back and increments it.

--- test/xbee_test.py
+++ test/xbee_test.py
@@ -2,22 +2,37 @@
 
 from serial import Serial, serialutil
 
+def getNum(s):
+    s_new = ""
+    for c in s:
+        if (c != '\r' or c != '\n'): s_new += c
+    return s_new
+
 def main():
     with Serial('/dev/ttyUSB0', 9600) as xbee_dev:
         try:
-            # Send a simple message to the FPGA, then wait for response
-            bytes_written = xbee_dev.write("Hello World")
-            # Wait for the response
-            message = xbee_dev.read(len("Hello World"))
-            print message
-            print "Test Successful. Exiting now"
+            message = '1'
+            # Start the hot potato with 1
+            xbee_dev.write(message)
+            i = 1
+
+            while True:
+                # Wait for the response (should be 1 greater than before)
+                print "Sent:", message
+                message = int(getNum(xbee_dev.readline()))
+                print "Recieved:", message, "\n"
+                message += 1
+                # Send it back
+                xbee_dev.write(str(message))
+
         except serialutil.SerialException as e:
             print "Test Failed! The xbee could not be found"
             print "Make sure that the device is properly connected"
-            print "Error message:"
-            print e
         except KeyboardInterrupt:
-            print "Test Failed! Interrupted by the user."
+            print "Interrupted by the user"
+        except Exception as e:
+            print "\nUnknown Error:"
+            print "\t", e
 
 if (__name__ == '__main__'):
     main()
