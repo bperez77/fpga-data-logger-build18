b2197bb4d1ae19877cefab24ff25acdb70b89b2c
cnordgren <cnordgre@andrew.cmu.edu>
2014-01-14 17:55:01 -0500

    Started pwm interface

--- dev/fpga/pwm.sv
+++ dev/fpga/pwm.sv
@@ -3,9 +3,18 @@
 module pwm
   #(PARAMETER FREQ = 100)
    (input logic [7:0] duty_cycle,
+    input logic en, clk, clr,
     output logic out);
 
-   pipo_reg dc(
+   logic [7:0] 	 dc_out, segments;
+   
+   pipo_reg dc(.D(duty_cycle), .Q(dc_out), .clk(clk), .clr(clr));
+   counter seg_counts(.en(), .Q(segments), .clk(clk), .clr(clr));
+   counter clock_counts(.en(1'b1), 
+
+
+   assign out = dc_out > segments;
+   
 
 
 endmodule: pwm
