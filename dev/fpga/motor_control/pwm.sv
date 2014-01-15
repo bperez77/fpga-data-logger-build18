53d8e6d29fa0b3781322c7888ef01f36f7771d0e
Rohit Banerjee <rohitban@ece002.ece.cmu.edu>
2014-01-15 15:56:46 -0500

    Updated the default values and module ports

--- dev/fpga/motor_control/pwm.sv
+++ dev/fpga/motor_control/pwm.sv
@@ -4,14 +4,14 @@ module pwm
   #(parameter FREQ = 17'd100_000, FREQ_BITS = 17)
    (input logic [6:0] duty_cycle,
     input logic en, clk, clr,
-    output logic out,
-    output logic [FREQ_BITS-1:0] clock_count,
-    output logic [6:0] segments);
+    output logic out);
+    //output logic [FREQ_BITS-1:0] clock_count,
+    //output logic [6:0] segments);
 
    logic seg_inc; 
    logic [6:0] dc_out; 
-   //logic [6:0] segments;
-   //logic [FREQ_BITS-1:0] clock_count;
+   logic [6:0] segments;
+   logic [FREQ_BITS-1:0] clock_count;
    
    assign seg_inc = (clock_count == FREQ);
 
