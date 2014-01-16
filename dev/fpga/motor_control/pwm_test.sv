7aabff0182d24bbe1a88354851b34b31ead20e2d
Rohit Banerjee <rohitban@hebe.lab.ece.cmu.local>
2014-01-15 21:57:35 -0500

    Updated the test to check a wider range

--- dev/fpga/motor_control/pwm_test.sv
+++ dev/fpga/motor_control/pwm_test.sv
@@ -1,28 +1,46 @@
 
 
 module pwm_testbench;
-  logic clk, clr, en, out;
-  logic [6:0] duty_cycle;
+  logic clk, clr, load, en, pwm_pulse;
+  logic [15:0] duty_cycle;
 
-  pwm #(7'd10,7) p1(.*);
+  pwm #(16'd500, 16'd5) p2(.*);
 
   initial begin
-  $monitor($time, " clock_count=%d, clr=%b, en=%b, duty_cycle=%d, segments=%d, out=%b",
-                  p1.clock_count, clr, en, duty_cycle, p1.segments, out);
+   $monitor($time, " clock_count=%d, clr=%b, duty_cycle=%d, pwm_pulse=%b",
+                  p2.clock_count, clr,  duty_cycle, pwm_pulse);
   clk = 0;
   forever #1 clk = ~clk;
   end
 
   initial begin
   duty_cycle = 7'd90;
-  en <= 0;
-  clr <= 1;
+  clr <= 1'd1;
+  en <= 1'd0;
   @(posedge clk);
-  clr <= 0;
-  en <= 1;
+  clr <= 1'd0;
+  en <= 1'd1;
   @(posedge clk);
-  en <= 0;
+  en <= 1'd0;
   @(posedge clk);
+  #5000;
+  duty_cycle <= 7'd50;
+  en <= 1'b1;
+  @(posedge clk);
+  en <= 1'b0;
+  @(posedge clk);
+  #5000;
+  duty_cycle <= 7'd10;
+  en <= 1'b1;
+  @(posedge clk);
+  en <= 1'b0;
+  @(posedge clk);
+  #5000;
+  duty_cycle <= 7'd70;
+  en <= 1'b1;
+  @(posedge clk);
+  en <= 1'b0;
+  @(posedge clk); 
   #5000;
   $finish;
   end
