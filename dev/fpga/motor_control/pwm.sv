6927d4dc46b1fc5dace299645cad59373ea658f6
bmperez <bmperez@seventy7.(none)>
2014-02-21 18:09:29 -0500

    Reworked the directory structure, deleted old files

--- dev/fpga/motor_control/pwm.sv
+++ dev/fpga/motor_control/pwm.sv
@@ -1,25 +1,63 @@
-//PWM Controller SystemVerilog description
-
 module pwm
-  #(parameter FREQ = 17'd100_000, FREQ_BITS = 17)
-   (input logic [6:0] duty_cycle,
-    input logic en, clk, clr,
-    output logic out);
+  #(parameter CLOCK = 50000000, WIDTH = 8, FREQ = 100)
+   (input logic [WIDTH-1:0] duty_cycle, 
+    input logic clr, clk, 
+    output logic pwm, new_dc);
 
-   logic seg_inc; 
-   logic [6:0] dc_out; 
-   logic [6:0] segments;
-   logic [FREQ_BITS-1:0] clock_count;
+   localparam PULSE_SIZE = CLOCK / FREQ;
+   localparam CHUNK_SIZE = PULSE_SIZE / (2**WIDTH);
+   localparam MAX_CHUNKS = 2**WIDTH-1;
    
-   assign seg_inc = (clock_count == FREQ);
+   logic [$clog2(CHUNK_SIZE)-1:0] clocks;
+   logic [WIDTH-1:0] 	     chunk_count;
+   logic 		     chunk;
 
-   pipo_reg #(7) dc(.D(duty_cycle), .Q(dc_out), .clk(clk), .clr(clr),.load(en));
+   limit_counter #(.WIDTH($clog2(CHUNK_SIZE)), .LIMIT(CHUNK_SIZE)) clk_cnt(.en(1'b1), .clr(clr), .clk(clk), .Q(clocks));
 
-   limit_counter #(FREQ_BITS) clock_counts(.en(1'b1), .Q(clock_count), 
-                                         .clk(clk), .clr(clr),.limit(FREQ));
+   limit_counter #(.WIDTH(WIDTH), .LIMIT(MAX_CHUNKS)) chnk_cnt(.en(chunk), .clr(clr), .clk(clk), .Q(chunk_count));
 
-   limit_counter #(7) seg_counts(.en(seg_inc), .Q(segments), .clk(clk), .clr(clr), .limit(7'd100));
-
-   assign out = dc_out > segments;  
-
+   assign chunk = (clocks == CHUNK_SIZE-1);
+   assign pwm = (chunk_count < duty_cycle);
+   assign new_dc = chunk_count == MAX_CHUNKS - 2;
+   
 endmodule: pwm
+
+/*module pwm_test();
+   logic [7:0] duty_cycle;
+   logic       clr, clk, pwm;
+
+   initial begin
+      clk = 1'b0;
+      forever #5 clk = ~clk;
+   end
+
+   pwm #(.CLOCK(100000)) pulse(.*);
+
+   initial begin
+      $monitor($time,, 
+	       "clocks=%d, pwm=%b, duty_cycle=%d, chunk=%b, chunk_count=%d", 
+	       pulse.clocks, pwm, duty_cycle, pulse.chunk, pulse.chunk_count);
+      @(posedge clk);
+      clr <= 1'b1;
+      duty_cycle <= 8'd128;
+      @(posedge clk);
+      @(posedge clk);
+      clr <= 1'b0;
+      @(posedge clk);
+      @(posedge clk);
+      for(int i = 0; i < 5000; i++)
+	@(posedge clk);
+      duty_cycle <= 8'd64;
+      for(int i = 0; i <5000; i++)
+	@(posedge clk);
+      @(posedge clk);
+      $display("pulse size = %d", pulse.PULSE_SIZE);
+      $display("Chunk size = %d", pulse.CHUNK_SIZE);
+      $display("Max chunks = %d", pulse.MAX_CHUNKS);
+      #4
+      $finish;
+   end // initial begin
+
+endmodule: pwm_test */
+
+      
--- test/simulation/pwm_tester.sv
+++ test/simulation/pwm_tester.sv
@@ -1,28 +1,4 @@
-module pwm
-  #(parameter CLOCK = 50000000, WIDTH = 8, FREQ = 100)
-   (input logic [WIDTH-1:0] duty_cycle, 
-    input logic clr, clk, 
-    output logic pwm, new_dc);
-
-   localparam PULSE_SIZE = CLOCK / FREQ;
-   localparam CHUNK_SIZE = PULSE_SIZE / (2**WIDTH);
-   localparam MAX_CHUNKS = 2**WIDTH-1;
-   
-   logic [$clog2(CHUNK_SIZE)-1:0] clocks;
-   logic [WIDTH-1:0] 	     chunk_count;
-   logic 		     chunk;
-
-   limit_counter #(.WIDTH($clog2(CHUNK_SIZE)), .LIMIT(CHUNK_SIZE)) clk_cnt(.en(1'b1), .clr(clr), .clk(clk), .Q(clocks));
-
-   limit_counter #(.WIDTH(WIDTH), .LIMIT(MAX_CHUNKS)) chnk_cnt(.en(chunk), .clr(clr), .clk(clk), .Q(chunk_count));
-
-   assign chunk = (clocks == CHUNK_SIZE-1);
-   assign pwm = (chunk_count < duty_cycle);
-   assign new_dc = chunk_count == MAX_CHUNKS - 2;
-   
-endmodule: pwm
-
-/*module pwm_test();
+module pwm_test();
    logic [7:0] duty_cycle;
    logic       clr, clk, pwm;
 
@@ -58,6 +34,6 @@ endmodule: pwm
       $finish;
    end // initial begin
 
-endmodule: pwm_test */
+endmodule: pwm_test
 
-      
+ 
